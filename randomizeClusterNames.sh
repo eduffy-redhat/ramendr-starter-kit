9d52109ac825bb2e861e2eaccc61efc84cb0e07a
Evan Duffy <eduffy@redhat.com>
2026-09-18 12:57:11 -0400

    Fix randomizer script, global ID

--- randomizeClusterNames.sh
+++ randomizeClusterNames.sh
@@ -1,7 +1,8 @@
 #!/bin/env bash
 set -euo pipefail
 
+ID="$(dd if=/dev/urandom bs=256 count=1 | md5sum | cut -c -5)"
 for f in $(grep -r "\(eduffy-\)\S*\(-primary\|-secondary\)" --include="*.yaml" | cut -d ':' -f 1 | sort -u); do
-	sed -e "s/\(eduffy-\)\S*\(-primary\|-secondary\)/\1$(dd if=/dev/urandom bs=256 count=1 | md5sum | cut -c -5)\2/g" -i "$f"
+	sed -e "s/\(eduffy-\)\S*\(-primary\|-secondary\)/\1${ID}\2/g" -i "$f"
 done
 # sed -e "s/\(eduffy-\)[0-9a-z]\{5\}-/\1$(dd if=/dev/urandom bs=256 count=1 | md5sum | cut -c -5)-/g" -i overrides/values-cluster-names.yaml
