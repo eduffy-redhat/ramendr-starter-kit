c93c009490d75dc68a9bd811525ad21cbeb1779a
Evan Duffy <eduffy@redhat.com>
2026-09-18 12:54:32 -0400

    Fix randomizer script to rename every occurance

--- randomizeClusterNames.sh
+++ randomizeClusterNames.sh
@@ -1,4 +1,7 @@
 #!/bin/env bash
 set -euo pipefail
 
-sed -e "s/\(eduffy-\)[0-9a-z]\{5\}-/\1$(dd if=/dev/urandom bs=256 count=1 | md5sum | cut -c -5)-/g" -i overrides/values-cluster-names.yaml
+for f in $(grep -r "\(eduffy-\)\S*\(-primary\|-secondary\)" --include="*.yaml" | cut -d ':' -f 1 | sort -u); do
+	sed -e "s/\(eduffy-\)\S*\(-primary\|-secondary\)/\1$(dd if=/dev/urandom bs=256 count=1 | md5sum | cut -c -5)\2/g" -i "$f"
+done
+# sed -e "s/\(eduffy-\)[0-9a-z]\{5\}-/\1$(dd if=/dev/urandom bs=256 count=1 | md5sum | cut -c -5)-/g" -i overrides/values-cluster-names.yaml
